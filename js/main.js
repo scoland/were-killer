1f49aaf01630fac05633c40af976a3c74e76b2fd
Sean Colandrea <link50@gmail.com>
2013-04-16 19:50:29 -0500

    Add simple accordion jQuery

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+$(document).ready(function() {
+	var allPanels = $(".fan-content").hide();
+	$('#fan > section > h2 > a').click(function(event) {
+	  event.preventDefault();
+	  $this = $(this);
+      $target =  $this.parent().next();
 
+      if(!$target.hasClass('active')){
+         allPanels.removeClass('active').slideUp();
+         $target.addClass('active').slideDown();
+      }
+  	});
+});
